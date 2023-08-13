4e547275f6ba875aa035a18b97263a05743a9d5a
Joao Marques <johnmarques.br2002@gmail.com>
2023-08-12 20:41:35 -0400

    updated swagger for not only dev

--- PrizeWinnerAPI/Program.cs
+++ PrizeWinnerAPI/Program.cs
@@ -43,11 +43,8 @@ namespace PrizeWinnerAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
